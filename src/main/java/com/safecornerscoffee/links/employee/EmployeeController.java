958934ba5aa95bc4086d086de3a0dafaf8d7148f
safecorners <safeconers.mobile@gmail.com>
2021-09-21 16:34:04 +0900

    integrate hateoas(hypermedia as the engine of application state)

--- src/main/java/com/safecornerscoffee/links/employee/EmployeeController.java
+++ src/main/java/com/safecornerscoffee/links/employee/EmployeeController.java
@@ -1,36 +1,47 @@
 package com.safecornerscoffee.links.employee;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeController {
 
     private final EmployeeRepository repository;
+    private final EmployeeModelAssembler assembler;
 
-    public EmployeeController(EmployeeRepository repository) {
+    public EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     @GetMapping("/employees")
-    public List<Employee> all() {
-        return repository.findAll();
+    public CollectionModel<EntityModel<Employee>> all() {
+        List<Employee> employees = repository.findAll();
+        return CollectionModel.of(employees.stream().map(assembler::toModel).collect(Collectors.toList()),
+            linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 
     @PostMapping("/employees")
-    public Employee newEmployee(@RequestBody Employee newEmployee) {
-        return repository.save(newEmployee);
+    public EntityModel<Employee> newEmployee(@RequestBody Employee newEmployee) {
+        return assembler.toModel(repository.save(newEmployee));
     }
 
     @GetMapping("/employees/{id}")
-    public Employee one(@PathVariable Long id) {
-        return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    public EntityModel<Employee> one(@PathVariable Long id) {
+        Employee employee = repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+        return assembler.toModel(employee);
     }
 
     @PutMapping("/employees/{id}")
-    public Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
-        return repository.findById(id)
+    public EntityModel<Employee> replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+        Employee updatedEmployee = repository.findById(id)
                 .map(employee -> {
                     employee.setName(newEmployee.getName());
                     employee.setRole(newEmployee.getRole());
@@ -40,6 +51,8 @@ public class EmployeeController {
                    newEmployee.setId(id);
                    return repository.save(newEmployee);
                 });
+
+        return assembler.toModel(updatedEmployee);
     }
 
     @DeleteMapping("/employees/{id}")
