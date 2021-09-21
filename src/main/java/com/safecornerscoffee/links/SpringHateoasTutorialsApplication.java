91772b2c9213e2f4982f5b113595dd3710cc63df
safecorners <safeconers.mobile@gmail.com>
2021-09-21 15:09:25 +0900

    create by start.spring.io

---
+++ src/main/java/com/safecornerscoffee/links/SpringHateoasTutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.safecornerscoffee.links;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHateoasTutorialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHateoasTutorialsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/safecornerscoffee/links/SpringHateoasTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safecornerscoffee.links;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHateoasTutorialsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
