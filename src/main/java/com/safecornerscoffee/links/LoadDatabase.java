581f943cdbdb1b325f9fc07f198e6c1b8850fbf9
safecorners <safeconers.mobile@gmail.com>
2021-09-21 17:10:46 +0900

    create OrderRepository

--- src/main/java/com/safecornerscoffee/links/LoadDatabase.java
+++ src/main/java/com/safecornerscoffee/links/LoadDatabase.java
@@ -2,6 +2,9 @@ package com.safecornerscoffee.links;
 
 import com.safecornerscoffee.links.employee.Employee;
 import com.safecornerscoffee.links.employee.EmployeeRepository;
+import com.safecornerscoffee.links.order.Order;
+import com.safecornerscoffee.links.order.OrderRepository;
+import com.safecornerscoffee.links.order.OrderStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -14,10 +17,18 @@ public class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
-    public CommandLineRunner initDatabase(EmployeeRepository repository) {
+    public CommandLineRunner initDatabase(EmployeeRepository employeeRepository, OrderRepository orderRepository) {
+
         return args -> {
-            log.info("Preloading " + repository.save(new Employee("Emma", "Stone", "Actress")));
-            log.info("Preloading " + repository.save(new Employee("Scarlett", "Johansson", "Actress")));
+            employeeRepository.save(new Employee("Emma", "Stone", "Actress"));
+            employeeRepository.save(new Employee("Scarlett", "Johansson", "Actress"));
+
+            employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
+
+            orderRepository.save(new Order("MBP M1", OrderStatus.COMPLETED));
+            orderRepository.save(new Order("MBP Intel", OrderStatus.IN_PROGRESS));
+
+            orderRepository.findAll().forEach(order -> log.info("Preloaded " + order));
         };
     }
 }
